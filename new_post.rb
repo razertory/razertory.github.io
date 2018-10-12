272cf2a9fbbafa28d16d34ef142453f9807535e1
Razertory <razertory@163.com>
2018-10-13 00:39:35 +0800

    add a script

---
+++ new_post.rb
@@ -0,0 +1,31 @@
+require 'date'
+require 'colorize'
+require 'pathname'
+
+file = ARGV[0].to_s
+
+if file == ""
+  puts "Please input file name!".red
+  exit
+end
+
+content =
+"---
+layout: post
+title: \"#{file}\"
+date: #{Time.now}
+categories:
+---"
+
+path = Pathname.new(File.dirname(__FILE__)).realpath
+file_name = "#{path}/_posts/#{Date.today}-#{ARGV[0]}.md"
+
+begin
+  file = File.new(file_name, "w")
+  file.write(content)
+  puts "Your post `#{file_name}` has been created".green
+rescue
+   File.delete(file_name)
+ensure
+  file.close
+end
